f9b5880407df7133924448d39f4e66b6d9905714
dhanyahari07 <dhanyahari07@gmail.com>
2021-10-09 15:22:15 +0530

    live data

--- MyLiveDataApplication/app/src/main/java/com/example/mylivedataapplication/MainActivity.java
+++ MyLiveDataApplication/app/src/main/java/com/example/mylivedataapplication/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.mylivedataapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView tv;
+    Button random_nu;
     private String TAG=this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +21,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv=findViewById(R.id.tv);
+        random_nu=findViewById(R.id.random_no);
+
+
         //MainActivityDataGenerator myData=new MainActivityDataGenerator();
         MainActivityViewModel viewModel= new ViewModelProvider(this).get(MainActivityViewModel.class);
         //String myRandomNumber=myData.getNumber();
-        String myRandomNumber=viewModel.getNumber();
-        tv.setText(myRandomNumber);
+        LiveData<String> myRandomNumber=viewModel.getNumber();
+        myRandomNumber.observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                tv.setText(s);
+            }
+        });
+        random_nu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewModel.createNumber();
+            }
+        });
+
         Log.i(TAG,"random number set");
+
     }
 }
--- MyLiveDataApplication/app/src/main/java/com/example/mylivedataapplication/MainActivityViewModel.java
+++ MyLiveDataApplication/app/src/main/java/com/example/mylivedataapplication/MainActivityViewModel.java
@@ -2,6 +2,7 @@ package com.example.mylivedataapplication;
 
 import android.util.Log;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import java.util.Random;
@@ -9,22 +10,23 @@ import java.util.Random;
 public class MainActivityViewModel extends ViewModel {
 
     private String TAG=this.getClass().getSimpleName();
-    private String myRandomNumber;
+    private MutableLiveData<String> myRandomNumber;
 
-    public String getNumber(){
+    public MutableLiveData<String> getNumber(){
         Log.i(TAG,"get number");
         if(myRandomNumber==null)
         {
+            myRandomNumber=new MutableLiveData<>();
             createNumber();
         }
         return myRandomNumber;
 
     }
 
-    private void createNumber(){
-        Log.i(TAG,"cretae new number");
+    public void createNumber(){
+        Log.i(TAG,"create new number");
         Random random=new Random();
-        myRandomNumber="Number"+(random.nextInt(10-1)+1);
+        myRandomNumber.setValue("Number"+(random.nextInt(10-1)+1));
     }
 
     @Override
