19ddbd2f5722862619c4befbcf1bb2c3fef9bbf8
dhanyahari07 <dhanyahari07@gmail.com>
2021-10-09 11:02:55 +0530

    view model

--- MyViewModelApplication/app/src/main/java/com/example/myviewmodelapplication/MainActivity.java
+++ MyViewModelApplication/app/src/main/java/com/example/myviewmodelapplication/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.myviewmodelapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv;
+    private String TAG=this.getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.tv);
+        //MainActivityDataGenerator myData=new MainActivityDataGenerator();
+        MainActivityDataGenerator viewModel= new ViewModelProvider(this).get(MainActivityDataGenerator.class);
+        //String myRandomNumber=myData.getNumber();
+        String myRandomNumber=viewModel.getNumber();
+        tv.setText(myRandomNumber);
+        Log.i(TAG,"random number set");
     }
 }
---
+++ MyViewModelApplication/app/src/main/java/com/example/myviewmodelapplication/MainActivityDataGenerator.java
@@ -0,0 +1,34 @@
+package com.example.myviewmodelapplication;
+
+import android.util.Log;
+
+import androidx.lifecycle.ViewModel;
+
+import java.util.Random;
+
+public class MainActivityDataGenerator extends ViewModel {
+    private String TAG=this.getClass().getSimpleName();
+    private String myRandomNumber;
+
+    public String getNumber(){
+        Log.i(TAG,"get number");
+        if(myRandomNumber==null)
+        {
+            createNumber();
+        }
+        return myRandomNumber;
+
+    }
+
+    private void createNumber(){
+        Log.i(TAG,"cretae new number");
+        Random random=new Random();
+        myRandomNumber="Number"+(random.nextInt(10-1)+1);
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        Log.i(TAG,"View model destroyed");
+    }
+}
