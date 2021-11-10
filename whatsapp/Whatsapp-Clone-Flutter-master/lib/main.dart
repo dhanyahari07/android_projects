99f353f49e9267052ef33cb083a69b207def390e
dhanyahari07 <dhanyahari07@gmail.com>
2021-11-10 14:07:40 +0530

    whatsapp clone

---
+++ whatsapp/Whatsapp-Clone-Flutter-master/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'Screens/CameraScreen.dart';
+import 'Screens/LoginScreen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          fontFamily: "OpenSans",
+          primaryColor: Color(0xFF075E54),
+          accentColor: Color(0xFF128C7E)),
+      home: LoginScreen(),
+    );
+  }
+}
