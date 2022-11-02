09c45ebac25b84671ea127a5ae11e40d29f2ba9c
dhanyahari07 <dhanyahari07@gmail.com>
2022-11-02 14:17:52 +0530

    tabview

---
+++ TabView/app/src/main/java/com/example/mytabviewapplication/Fragment1.java
@@ -0,0 +1,23 @@
+package com.example.mytabviewapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment1 extends Fragment {
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+}
---
+++ TabView/app/src/main/java/com/example/mytabviewapplication/Fragment2.java
@@ -0,0 +1,21 @@
+package com.example.mytabviewapplication;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Fragment2 extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_2, container, false);
+    }
+}
